2b28645aa8cc098a6008ce724e4961ea782c4276
shunoiazad <78991492+shunoiazad@users.noreply.github.com>
2021-06-04 15:57:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,130 +1,76 @@
 import discord
 from discord.ext import commands
-
-import random
-import asyncio
-import datetime
-import pytz
-
 import os
-from keep_alive import keep_alive
+import json
+import asyncio
 
-embed_color = 0x616060
 
-intents = discord.Intents.default()
-intents.members = True
+with open('config.json', 'r') as f:
+    config = json.load(f)
 
-bot = commands.Bot(command_prefix='n!', intents=intents, help_command=None)
+bot = commands.Bot(command_prefix=config['bot_prefix'])
 
-#onready---
+#remove default help
+bot.remove_command('help')
+
+
+#on ready
 @bot.event
 async def on_ready():
-    print(f'Successfully logged in as: {bot.user.name}')
+    print(f"Logged in as {bot.user.name}")
 
 
-#LOAD COMMANDS
-@bot.command()
+#load
+@bot.command(name='load')
+@commands.has_permissions(administrator=True)
+async def load_cog(ctx, extension):
+    try:
+        cog = f"cogs.{extension.lower()}"
+        bot.load_extension(cog)
+        await ctx.message.add_reaction('✅')
+    except Exception as e:
+        await ctx.reply(f'```{e}```')
+
+
+#unload
+@bot.command(name='unload')
+@commands.has_permissions(administrator=True)
+async def unload_cog(ctx, extension):
+    try:
+        cog = f"cogs.{extension.lower()}"
+        bot.unload_extension(cog)
+        await ctx.message.add_reaction('✅')
+    except Exception as e:
+        await ctx.reply(f'```{e}```')
+
+
+#reload
+@bot.command(name='reload')
+@commands.has_permissions(administrator=True)
+async def reload_cog(ctx, extension):
+    try:
+        cog = f"cogs.{extension.lower()}"
+        bot.unload_extension(cog)
+        bot.load_extension(cog)
+        await ctx.message.add_reaction('✅')
+    except Exception as e:
+        await ctx.reply(f'```{e}```')
+
+
+#refresh
+@bot.command(name='refresh')
 @commands.is_owner()
-async def enable(ctx, extension):
-    if ctx.author.id == 386860764368994304:
-        try:
-            if extension == "all":
-                for f in os.listdir('./myCogs'):
-                    if f.endswith('.py'):
-                        exempted_files = ["Snipe_Command.py"]
-                        if f in exempted_files:
-                            pass
-
-                        else:
-                            bot.load_extension(f'myCogs.{f[:-3]}')
-                            await ctx.message.add_reaction("✅")
-                            
-                    else:
-                        pass
-            else:
-                bot.load_extension(f'myCogs.{extension}')
-                await ctx.message.add_reaction("✅")
-
-        except Exception as error:
-            await ctx.message.reply(
-              content = '''An error occured: ```{}```'''.format(error)
-          )
-          
-    else:
-        await ctx.send(embed=discord.Embed(
-            title = "You don't have the permission to use this command",
-            color = embed_color
-        ))
-
-@bot.command()
-@commands.is_owner()
-async def refresh(ctx, extension):
-    if ctx.author.id == 386860764368994304 or 657181824673513482:
-        try:
-            if extension == "all":
-                for f in os.listdir('./myCogs'):
-                    if f.endswith('.py'):
-                        exempted_files = ["Snipe_Command.py"]
-                        if f in exempted_files:
-                            pass
-
-                        else:
-                            bot.unload_extension(f'myCogs.{f[:-3]}')
-                            await asyncio.sleep(0.1)
-                            bot.load_extension(f'myCogs.{f[:-3]}')
-                            await ctx.message.add_reaction("✅")
-                            
-
-                    else:
-                        pass
-            else:
-                bot.unload_extension(f'myCogs.{extension}')
-                await asyncio.sleep(0.1)
-                bot.load_extension(f'myCogs.{extension}')
-                await ctx.message.add_reaction("✅")
-
-        except Exception as error:
-            await ctx.message.reply(
-              content = '''An error occured: ```{}```'''.format(error)
-          )
-          
-    else:
-        await ctx.send(embed=discord.Embed(
-            title = "You don't have the permission to use this command",
-            color = embed_color
-        ))
-
-@bot.command()
-@commands.is_owner()
-async def disable(ctx, extension):
-    if ctx.author.id == 386860764368994304:
-        try:
-            if extension == "all":
-                for f in os.listdir('./myCogs'):
-                    if f.endswith('.py'):
-                        exempted_files = []
-                        if f in exempted_files:
-                            pass
-                        else:
-                            bot.unload_extension(f'myCogs.{f[:-3]}')
-                            await ctx.message.add_reaction("✅")
-                            
-                    else:
-                        pass
-            else:
-                bot.unload_extension(f'myCogs.{extension}')
-                await ctx.message.add_reaction("✅")
-
-        except Exception as error:
-            await ctx.message.reply(
-              content = '''An error occured: ```{}```'''.format(error)
-          )
-          
-    else:
-        await ctx.send(embed=discord.Embed(
-            title = "You don't have the permission to use this command",
-            color = embed_color
-        ))
+async def refresh(ctx):
+    for f in os.listdir('./cogs'):
+        if f.endswith('.py') and not f in config["exempted_cogs"]:
+            try:
+                cog = f'cogs.{f[0:-3]}'
+                bot.load_extension(cog)
+                bot.unload_extension(cog)
+                bot.load_extension(cog)
+            except:
+                pass
+    await ctx.message.add_reaction('✅')
 
 
 
@@ -132,38 +78,363 @@ async def disable(ctx, extension):
 
 
 
-@bot.command(aliases=['cmds','help'])
-async def commands(ctx):
+
+
+
+
+
+
+
+#HELP_CMD
+@bot.group(name="help", aliases=["cmds", "commands"], invoke_without_command=True)
+async def help_command(ctx):
     mod_embed = discord.Embed(
         title = "Commands list",
-        color = embed_color
+        color = int(config["embed_color"], 16)
     )
     mod_embed.add_field(
         name = 'Tickets',
-        value = "`n!close` - closes a ticket, minor purpose channels",
+        value = "`close`",
         inline = False
     )
     mod_embed.add_field(
         name = "Channels",
-        value = "`n!purge <optional amount>` - bulk deletes messages in a channel, default is 5\n`n!lockdown` - locks the channel the command is executed in\n`n!unlock` - unlocks the channel the command is executed in",
+        value = "`purge`, `lockdown`",
         inline = False
     )
     mod_embed.add_field(
         name = "Mutes",
-        value = "`n!mute <user> <optional amount> <optional unit>` - mutes the specified user unconditionally or temporarily\n`n!unmute <user>` - unmuted the specified user",
+        value = "`mute`, `unmute`",
         inline = False
     )
     mod_embed.add_field(
         name = "Warns",
-        value = "`n!warn <user> <reason>` - warns the specified member\n`n!removewarn <user> <reference>` - removes the warn from the specific warn reference of the user\n`n!clearwarn <user>` - clears all the warn from the user",
+        value = "`warn`, `removewarn`, `clearwarns`",
         inline = False
     )
     mod_embed.add_field(
         name = "Advanced Moderation",
-        value = "`n!kick <user> <optional reason>` - kicks the specified member\n`n!softban <user> <optional reason>` - kicks the user and deletes all of their message in the server\n`n!ban <user> <reason>` - bans the specified member\n`n!unban <userid> <optional moderator note>` - unbans the specified user ID holder",
+        value = "`kick`, `softban`, `ban`, `unban`, `email`",
         inline = False
     )
-    await ctx.send(embed=mod_embed)
+    mod_embed.set_footer(
+        text = "Use n!help <command>, to see the specified command's information and usage"
+    )
+    try:
+        await ctx.send(embed=mod_embed)
+    except Exception as e:
+        await ctx.reply(f'```{e}```')
+
+
+
+#HELP SUB CMDS
+#CLOSE_CMD
+@help_command.command(name="close")
+async def close_command(ctx):
+    embed = discord.Embed(
+        title = "Close Command (UNAVAILABLE)",
+        color = int(config["embed_color"], 16)
+    )
+    embed.add_field(
+        name = "Description",
+        value = "Closes minor purposed channels such as tickets.",
+        inline = False
+    )
+    embed.add_field(
+        name = "Usage",
+        value = "`n!close`",
+        inline = False
+    )
+    try:
+        await ctx.send(embed=embed)
+    except Exception as e:
+        await ctx.reply(f'```{e}```')
+
+#PURGE_CMD
+@help_command.command(name="purge")
+async def purge_command(ctx):
+    embed = discord.Embed(
+        title = "Purge Command",
+        color = int(config["embed_color"], 16)
+    )
+    embed.add_field(
+        name = "Description",
+        value = "Purges channel, basically clearing previous messages with the specified amount.",
+        inline = False
+    )
+    embed.add_field(
+        name = "Usage",
+        value = "`n!purge <amount / default=5>`",
+        inline = False
+    )
+    try:
+        await ctx.send(embed=embed)
+    except Exception as e:
+        await ctx.reply(f'```{e}```')
+
+#LOCKDOWN_CMD
+@help_command.command(name="lockdown")
+async def lockdown_command(ctx):
+    embed = discord.Embed(
+        title = "Lockdown Command (UNAVAILABLE)",
+        color = int(config["embed_color"], 16)
+    )
+    embed.add_field(
+        name = "Description",
+        value = "This command is used in urgent situations. It locks and override the channel permissions for `@everyone` to `view_channel=false`.",
+        inline = False
+    )
+    embed.add_field(
+        name = "Usage",
+        value = "`n!lockdown`",
+        inline = False
+    )
+    try:
+        await ctx.send(embed=embed)
+    except Exception as e:
+        await ctx.reply(f'```{e}```')
+
+#MUTE_CMD
+@help_command.command(name="mute")
+async def mute_command(ctx):
+    embed = discord.Embed(
+        title = "Mute Command",
+        color = int(config["embed_color"], 16)
+    )
+    embed.add_field(
+        name = "Description",
+        value = "Mutes the specified user with the time given. It takes 2 arguments, which is the user and the time limit. `s` stands for second(s), `m` stands for minute(s), `h` stands for hour(s), and `d` stands for days(s). An example for time limits are `2m`, `12h` and `5d`.",
+        inline = False
+    )
+    embed.add_field(
+        name = "Usage",
+        value = "`n!mute <@user> <time limit>`",
+        inline = False
+    )
+    try:
+        await ctx.send(embed=embed)
+    except Exception as e:
+        await ctx.reply(f'```{e}```')
+
+#UNMUTE_CMD
+@help_command.command(name="unmute")
+async def unmute_command(ctx):
+    embed = discord.Embed(
+        title = "Unmute Command",
+        color = int(config["embed_color"], 16)
+    )
+    embed.add_field(
+        name = "Description",
+        value = "Removes the mute from the specified user.",
+        inline = False
+    )
+    embed.add_field(
+        name = "Usage",
+        value = "`n!unmute <@user>`",
+        inline = False
+    )
+    try:
+        await ctx.send(embed=embed)
+    except Exception as e:
+        await ctx.reply(f'```{e}```')
+
+#WARN_CMD
+@help_command.command(name="warn")
+async def warn_command(ctx):
+    embed = discord.Embed(
+        title = "Warn Command",
+        color = int(config["embed_color"], 16)
+    )
+    embed.add_field(
+        name = "Description",
+        value = "Warns the specified user and a reason must be provided.",
+        inline = False
+    )
+    embed.add_field(
+        name = "Usage",
+        value = "`n!warn <@user> <reason>`",
+        inline = False
+    )
+    try:
+        await ctx.send(embed=embed)
+    except Exception as e:
+        await ctx.reply(f'```{e}```')
+
+#WARNINGS_CMD
+@help_command.command(name="warnings")
+async def warnings_command(ctx):
+    embed = discord.Embed(
+        title = "Warnings Command",
+        color = int(config["embed_color"], 16)
+    )
+    embed.add_field(
+        name = "Description",
+        value = "Shows every warnings of the user.",
+        inline = False
+    )
+    embed.add_field(
+        name = "Usage",
+        value = "`n!warnings <@user>`",
+        inline = False
+    )
+    try:
+        await ctx.send(embed=embed)
+    except Exception as e:
+        await ctx.reply(f'```{e}```')
+
+#REMOVEWARN_CMD
+@help_command.command(name="removewarn", aliases=["delwarn"])
+async def removewarn_command(ctx):
+    embed = discord.Embed(
+        title = "Removewarn Command",
+        color = int(config["embed_color"], 16)
+    )
+    embed.add_field(
+        name = "Description",
+        value = "Removes the warning of a user with an specific warn reference.",
+        inline = False
+    )
+    embed.add_field(
+        name = "Usage",
+        value = "`n!removewarn <@user> <warn reference>`",
+        inline = False
+    )
+    try:
+        await ctx.send(embed=embed)
+    except Exception as e:
+        await ctx.reply(f'```{e}```')
+
+#CLEARWARNS_CMD
+@help_command.command(name="clearwarns")
+async def clearwarns_command(ctx):
+    embed = discord.Embed(
+        title = "Clearwarn Command",
+        color = int(config["embed_color"], 16)
+    )
+    embed.add_field(
+        name = "Description",
+        value = "Clears all of the warning of the specified user.",
+        inline = False
+    )
+    embed.add_field(
+        name = "Usage",
+        value = "`n!clearwarns <@user>`",
+        inline = False
+    )
+    try:
+        await ctx.send(embed=embed)
+    except Exception as e:
+        await ctx.reply(f'```{e}```')
+        
+#KICK_CMD
+@help_command.command(name="kick")
+async def kick_command(ctx):
+    embed = discord.Embed(
+        title = "Kick Command",
+        color = int(config["embed_color"], 16)
+    )
+    embed.add_field(
+        name = "Description",
+        value = "Kicks the specified user out of the server.",
+        inline = False
+    )
+    embed.add_field(
+        name = "Usage",
+        value = "`n!kick <@user> <optional reason>`",
+        inline = False
+    )
+    try:
+        await ctx.send(embed=embed)
+    except Exception as e:
+        await ctx.reply(f'```{e}```')
+
+#SOFTBAN_CMD
+@help_command.command(name="softban")
+async def softban_command(ctx):
+    embed = discord.Embed(
+        title = "Softban Command",
+        color = int(config["embed_color"], 16)
+    )
+    embed.add_field(
+        name = "Description",
+        value = "Kicks the specified user and deletes all of their messages on the server.",
+        inline = False
+    )
+    embed.add_field(
+        name = "Usage",
+        value = "`n!softban <@user> <optional reason>`",
+        inline = False
+    )
+    try:
+        await ctx.send(embed=embed)
+    except Exception as e:
+        await ctx.reply(f'```{e}```')
+
+#BAN_CMD
+@help_command.command(name="ban")
+async def ban_command(ctx):
+    embed = discord.Embed(
+        title = "Ban Command",
+        color = int(config["embed_color"], 16)
+    )
+    embed.add_field(
+        name = "Description",
+        value = "Bans the specified user on the server.",
+        inline = False
+    )
+    embed.add_field(
+        name = "Usage",
+        value = "`n!ban <@user> <reason>`",
+        inline = False
+    )
+    try:
+        await ctx.send(embed=embed)
+    except Exception as e:
+        await ctx.reply(f'```{e}```')
+
+#UNBAN_CMD
+@help_command.command(name="unban")
+async def unban_command(ctx):
+    embed = discord.Embed(
+        title = "Unban Command",
+        color = int(config["embed_color"], 16)
+    )
+    embed.add_field(
+        name = "Description",
+        value = "Unbans the specified user id on the server.",
+        inline = False
+    )
+    embed.add_field(
+        name = "Usage",
+        value = "`n!unban <user id>`",
+        inline = False
+    )
+    try:
+        await ctx.send(embed=embed)
+    except Exception as e:
+        await ctx.reply(f'```{e}```')
+
+#EMAIL_CMD
+@help_command.command(name="email")
+async def email_command(ctx):
+    embed = discord.Embed(
+        title = "Email Command (UNAVAILABLE)",
+        color = int(config["embed_color"], 16)
+    )
+    embed.add_field(
+        name = "Description",
+        value = "This command is used when unbanning users to notify them. You will be asked with some questions which regards to the email.",
+        inline = False
+    )
+    embed.add_field(
+        name = "Usage",
+        value = "`n!email`",
+        inline = False
+    )
+    try:
+        await ctx.send(embed=embed)
+    except Exception as e:
+        await ctx.reply(f'```{e}```')
 
 
 
@@ -175,36 +446,9 @@ async def commands(ctx):
 
 
 
-@bot.command()
-async def perms(ctx, member: discord.Member):
-    await ctx.send(member.permissions_in(ctx.channel))
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-for f in os.listdir("myCogs"):
-    if f.endswith(".py"):
-        excepted = []
-        if f in excepted:
-            pass
-
-        else:
-            bot.load_extension("myCogs.{}".format(f[:-3]))
-
-
-
-
-
-keep_alive()
-token = os.getenv("TOKEN")
-bot.run(token)
+bot.run(os.environ.get('TOKEN'))
